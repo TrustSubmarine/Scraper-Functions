c43f9dde0854da696f1c36b4a4ebb0444eda12c6
Aditya Bajaj <bajaj.aditya2204@gmail.com>
2023-06-23 22:52:43 +0530

    Add user agent rotation placeholder

--- functions/restful/all_reviews.py
+++ functions/restful/all_reviews.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
 # 64 pages max for this test example (handy later)
 URL = "https://www.amazon.in/Baseus-Screenbar-Adjustable-Brightness-Temperature/dp/B08CXL3YQ8/"
@@ -13,19 +14,49 @@ headers = {
     "Upgrade-Insecure-Requests": "1",
 }
 
+# used for rotation
+USER_AGENTS = ["", "", "", "", ""]
+
 
 def all_reviews(URL, headers):
-    BASE_URL = "https://www." + URL.split(".")[1] + "." + URL.split(".")[2].split("/")[0]
+    BASE_URL = (
+        "https://www." + URL.split(".")[1] + "." + URL.split(".")[2].split("/")[0]
+    )
 
     page = requests.get(URL, headers=headers)
     soup = BeautifulSoup(page.content, "html.parser")
 
     see_all_reviews_anchor = soup.find(attrs={"data-hook": "see-all-reviews-link-foot"})
 
-    all_reviews_URL = "https://www.amazon.in" + see_all_reviews_anchor["href"]
+    all_reviews_URL = BASE_URL + see_all_reviews_anchor["href"]
     all_reviews_URL_clean = "/".join(all_reviews_URL.split("/")[0:-1])
 
     reviews_page = requests.get(all_reviews_URL_clean, headers=headers)
     reviews_soup = BeautifulSoup(reviews_page.content, "html.parser")
 
     all_reviews_parse_tree_list = reviews_soup.find_all(attrs={"data-hook": "review"})
+
+    MAX_PAGES = 2
+    count = 1
+    while not reviews_soup.find(class_="a-disabled a-last") and count <= MAX_PAGES:
+        # single page reviews here
+        for rev in all_reviews_parse_tree_list:
+            print(rev.find(attrs={"data-hook": "review-title"}).get_text())
+            print(rev.find(attrs={"data-hook": "review-body"}).get_text())
+            print("")
+
+        time.sleep(1)
+
+        next_page_URL = reviews_soup.find("li", class_="a-last")
+        next_page_URL = "https://amazon.in" + next_page_URL.contents[0]["href"]
+
+        time.sleep(1)
+
+        reviews_soup = BeautifulSoup(
+            requests.get(next_page_URL, headers=headers).content, "html.parser"
+        )
+        all_reviews_parse_tree_list = reviews_soup.find_all(attrs={"data-hook": "review"})
+
+        count += 1
+
+all_reviews(URL, headers)
