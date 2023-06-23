09d02db188b45c7d2c9afb2b11af9324d6c52abd
Aditya Bajaj <bajaj.aditya2204@gmail.com>
2023-06-24 00:42:41 +0530

    Finalise for test deployment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import functions_framework
 from functions.restful.all_reviews import all_reviews_req
 
-
+# cloud functions entry point
 @functions_framework.http
 def get_all_reviews(request):
     return all_reviews_req(request)
