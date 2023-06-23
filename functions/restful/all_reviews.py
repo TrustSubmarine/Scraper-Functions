7670a8fe0f871eb29f49e0d8a96864a389880ac1
Aditya Bajaj <bajaj.aditya2204@gmail.com>
2023-06-23 22:43:01 +0530

    Add experimental version

---
+++ functions/restful/all_reviews.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+
+# 64 pages max for this test example (handy later)
+URL = "https://www.amazon.in/Baseus-Screenbar-Adjustable-Brightness-Temperature/dp/B08CXL3YQ8/"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "DNT": "1",
+    "Connection": "close",
+    "Upgrade-Insecure-Requests": "1",
+}
+
+
+def all_reviews(URL, headers):
+    BASE_URL = "https://www." + URL.split(".")[1] + "." + URL.split(".")[2].split("/")[0]
+
+    page = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    see_all_reviews_anchor = soup.find(attrs={"data-hook": "see-all-reviews-link-foot"})
+
+    all_reviews_URL = "https://www.amazon.in" + see_all_reviews_anchor["href"]
+    all_reviews_URL_clean = "/".join(all_reviews_URL.split("/")[0:-1])
+
+    reviews_page = requests.get(all_reviews_URL_clean, headers=headers)
+    reviews_soup = BeautifulSoup(reviews_page.content, "html.parser")
+
+    all_reviews_parse_tree_list = reviews_soup.find_all(attrs={"data-hook": "review"})
