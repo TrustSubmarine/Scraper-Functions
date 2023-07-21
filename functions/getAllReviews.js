b342df699f2d75f508d0b2e71836a640b9be026d
Aditya Bajaj <bajaj.aditya2204@gmail.com>
2023-07-22 00:25:13 +0530

    Initial AWS Lambda Fix Patch

--- functions/getAllReviews.js
+++ functions/getAllReviews.js
@@ -1,9 +1,10 @@
-const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
+
 
 // Function to get a random user agent
 function getRandomUserAgent() {
   const userAgents = [
-    "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1",
+    // "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
     // "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41",
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
@@ -17,7 +18,15 @@ function getRandomUserAgent() {
 async function getAllReviews(url, headers) {
   console.log(url);
   console.log(headers);
-  const browser = await puppeteer.launch();
+
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
+
   const page = await browser.newPage();
   
   // Set the user agent
@@ -31,18 +40,24 @@ async function getAllReviews(url, headers) {
   // Go to the main product page
   await page.goto(url);
 
+  page.title().then(title => console.log(title));
+  
   // TODO: why does this line break everything?
   // const html = await page.content();
-
+  
   
   // Extract the link for all reviews
   // TODO: why does this line sometimes work, and then other times just not?
-  const seeAllReviewsAnchor = await page.$eval('[data-hook="see-all-reviews-link-foot"]', anchor => anchor.href);
+  // const seeAllReviewsAnchor = await page.$eval('[data-hook="see-all-reviews-link-foot"]', anchor => anchor.href);
+  const seeAllReviewsAnchor = await page.$eval('.a-link-emphasis.a-text-bold', anchor => anchor.href);
+  // seeAllReviewsAnchor.then(i => console.log(i));
   console.log(seeAllReviewsAnchor);
 
 //   const baseUrl = `https://www.${url.split("/")[2]}`;
 //   const allReviewsUrl = baseUrl + seeAllReviewsAnchor;
   const allReviewsUrl = seeAllReviewsAnchor;
+  console.log(allReviewsUrl);
+  console.log("after all reviews url print")
   
   // Go to the all reviews page
   await page.goto(allReviewsUrl);
@@ -77,7 +92,11 @@ async function getAllReviews(url, headers) {
   
   await browser.close();
   
-  return JSON.stringify({ reviews: reviewsList }, null, 4);
+  const result = JSON.stringify({ reviews: reviewsList }, null, 4);
+
+  console.log(result);
+
+  return result;
 }
 
 module.exports = { getAllReviews };
--- reviews.js
+++ reviews.js
@@ -1,29 +1,41 @@
 const fns = require('./functions/getAllReviews');
 
-// Example usage
-const url = "https://www.amazon.in/Baseus-Screenbar-Adjustable-Brightness-Temperature/dp/B08CXL3YQ8/";
-const headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
-    "Accept-Encoding": "gzip, deflate",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "DNT": "1",
-    "Connection": "close",
-    "Upgrade-Insecure-Requests": "1",
-    "sec-ch-ua": '"Brave";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform-version": '"13.2.0"',
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "no-cors",
-    "sec-fetch-site": "same-origin",
-    "sec-gpc": "1"
-};
 
-fns.getAllReviews(url, headers)
-    .then(reviews => {
-        console.log(reviews);
-        // console.log(headers);
-        // console.log(url);
-    })
-    .catch(error => {
-        console.error(error);
-    });
+// Example usage
+// const url = "https://www.amazon.in/Baseus-Screenbar-Adjustable-Brightness-Temperature/dp/B08CXL3YQ8/";
+// const headers = {
+//     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
+//     "Accept-Encoding": "gzip, deflate",
+//     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+//     "DNT": "1",
+//     "Connection": "close",
+//     "Upgrade-Insecure-Requests": "1",
+//     "sec-ch-ua": '"Brave";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+//     "sec-ch-ua-mobile": "?0",
+//     "sec-ch-ua-platform-version": '"13.2.0"',
+//     "sec-fetch-dest": "empty",
+//     "sec-fetch-mode": "no-cors",
+//     "sec-fetch-site": "same-origin",
+//     "sec-gpc": "1"
+// };
+
+async function getReviews(event, context, callback) {
+    const url = event['url']
+    const headers = event['headers']
+    let res = "hi";
+    fns.getAllReviews(url, headers)
+        .then(reviews => {
+            res = reviews;
+        })
+        .catch(error => {
+            console.error(error);
+            throw error;
+        });
+    
+    return callback(null, res);
+    // TODO: Read up on callback here and why it makes everything work
+}
+
+// AWS Lambda handler export
+// exports.getReviews = getReviews;
+exports.handler = getReviews;
